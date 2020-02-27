ec0b0b23d19cf20701fb1aaf184cab70ea1a2384
Jazulienux <jazulienux@gmail.com>
2020-02-27 16:01:16 +0700

    Modul 3

---
+++ Modul 3/Muhammad Saifuddin Jazuli/src/component/HelloComponent.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './HelloComponent.css';
+
+const HelloComponent = () => {
+    return <p className="text-p">Ini Adalah Arrow Funciton Yang Ada Di Folder</p>
+}
+
+export default HelloComponent;
