04ce044f21aa8be58772b223de9eedb0ba945e12
Jazulienux <jazulienux@gmail.com>
2020-04-05 09:55:49 +0700

    Praktikum

---
+++ Modul 4/MuhammadSaifuddinJazuli/Tugas/src/component/StatefullComponent.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+class StateFullComponent extends React.Component {
+    render() {
+        return <p>Ini Adalah StatementFull Componen Dalam Folder</p>
+    }
+}
+
+export default StateFullComponent;
