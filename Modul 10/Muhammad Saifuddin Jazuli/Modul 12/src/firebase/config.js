4ab6f0488dffda08f85bf2fead11256641e7da52
Jazulienux <jazulienux@gmail.com>
2020-04-30 18:36:25 +0700

    CRUD Firebase Realtime

---
+++ Modul 10/Muhammad Saifuddin Jazuli/Modul 12/src/firebase/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyBOENpj2KTS8MkKRTfFIB7CNp_hyQh84xk',
+  authDomain: 'fir-login-9d381.firebaseapp.com',
+  databaseURL: 'https://fir-login-9d381.firebaseio.com',
+  projectId: 'fir-login-9d381',
+  storageBucket: 'fir-login-9d381.appspot.com',
+  messagingSenderId: '1060175418722',
+  appId: '1:1060175418722:web:175c249ffb3290660b883b',
+  measurementId: 'G-G29F6E4H6R',
+};
+
+export default firebaseConfig;
